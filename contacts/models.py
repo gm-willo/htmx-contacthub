b58564c93c5a23a21d0c2b8317e363c2af47a51e
gm-willo <gm-willo>
2026-02-09 17:51:32 +0100

    Create Contact model

--- contacts/models.py
+++ contacts/models.py
@@ -4,3 +4,23 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = (
+        models.EmailField()
+    )  # this can't be unique 'cause two different users can create a contact with the same email
+    created_at = models.DateField(auto_now_add=True)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="contacts"  # user.contacts.all()
+    )
+
+    class Meta:  
+        unique_together = (
+            "user",
+            "email",
+        )  # it is used to prevent a user to create two contacts with the same email
+
+    def __str__(self):
+        return f"{self.name} <{self.email}>"
