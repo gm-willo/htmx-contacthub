719c3ddbc35ee0208cc93ced26f458a7331941af
gm-willo <gianmarco.ninno@willo.it>
2026-02-11 14:39:12 +0100

    Add form to add new contacts

--- contacts/forms.py
+++ contacts/forms.py
@@ -1,1 +1,20 @@
 from django import forms
+from .models import Contact
+
+
+class ContactForm(forms.ModelForm):
+    name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={"class": "input w-full", "placeholder": "Contact Name"}
+        )
+    )
+
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={"class": "input w-full", "placeholder": "Email Address"}
+        )
+    )
+
+    class Meta:
+        model = Contact
+        fields = ("name", "email")
--- contacts/views.py
+++ contacts/views.py
@@ -2,17 +2,20 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 
+from contacts.forms import ContactForm
+
 
 @login_required
 def index(request):
     contacts = request.user.contacts.all().order_by("-created_at")
-    context = {"contacts": contacts}
+    context = {"contacts": contacts, "form": ContactForm()}
     return render(request, "contacts.html", context)
 
 
 @login_required
 def search_contacts(request):
     import time
+
     time.sleep(2)
     query = request.GET.get("search", "")
 
